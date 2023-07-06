93a4362e4cfbc271b5e637318c5a71dd83bd5e26
krisbobo <kiyongbohbo@gmail.com>
2023-07-07 00:44:38 +0100

    Add ceasar cipher function
    The function takes a string and a key and returns a ciphered key. It covers the edge case where we reach Y and Z, and y and z and loops back to the start of the alphabets. It equally returns spaces wherever they appear

--- challenge.rb
+++ challenge.rb
@@ -3,12 +3,21 @@ def caeser_cipher(string, key)
   str = string.encode('US-ASCII')
   str.encoding
   str.bytes.each do |char|
-    coded_char = char + key
-    puts coded_char 
-    result += coded_char.chr
+    if char == 32
+      result += 32.chr
+    else
+      coded_char = char + key
+      if coded_char > 90 && coded_char < 97
+        coded_char = 65  + (coded_char - 90) - 1
+      if coded_char > 122
+        coded_char = 97 + (coded_char - 122) - 1
+      end
+      end
+      result += coded_char.chr
+    end
   end
   result
 end
 
-cipher = caeser_cipher('Hello world', 2)
+cipher = caeser_cipher('The Quick Brown foX jumped ovEr the laZY dogS', 5)
 puts cipher
